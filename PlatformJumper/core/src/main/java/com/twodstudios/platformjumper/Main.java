389a6dbe46f3e2b4639baa95002a16464c409147
jobedev95 <m95jbnb@hotmail.com>
2025-01-13 20:54:09 +0100

    chore: remove old imports

--- PlatformJumper/core/src/main/java/com/twodstudios/platformjumper/Main.java
+++ PlatformJumper/core/src/main/java/com/twodstudios/platformjumper/Main.java
@@ -1,15 +1,8 @@
 package com.twodstudios.platformjumper;
 
 import com.badlogic.gdx.*;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Rectangle;
-import com.badlogic.gdx.utils.ScreenUtils;
-import com.badlogic.gdx.utils.Array;
 import com.twodstudios.platformjumper.screens.PlayScreen;
-
-import static com.badlogic.gdx.math.MathUtils.random;
-import static java.lang.Math.abs;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends Game {
